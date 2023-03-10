c4457c64fa5f732ad77ab49b0cb6dff825309bfb
rabbishakir <rabbishakir@gmail.com>
2023-03-10 12:32:15 -0500

    initial commit

---
+++ src/com/agileteach/seleniumtutorials/AppTesting.java
@@ -0,0 +1,7 @@
+package com.agileteach.seleniumtutorials;
+
+public class AppTesting {
+    public static void main(String[] args) {
+        System.out.println("This is the first selenium project");
+    }
+}
