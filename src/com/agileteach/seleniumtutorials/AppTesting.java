259cba15b6fbbf314b8df108c36455749c235241
rabbishakir <rabbishakir@gmail.com>
2023-03-10 13:25:50 -0500

    initial commit

--- src/com/agileteach/seleniumtutorials/AppTesting.java
+++ src/com/agileteach/seleniumtutorials/AppTesting.java
@@ -1,7 +1,17 @@
 package com.agileteach.seleniumtutorials;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AppTesting {
+
     public static void main(String[] args) {
-        System.out.println("This is the first selenium project");
+    System.setProperty("webdriver.chrome.driver", "C:\\webdriversrc\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://www.facebook.com");
+
+    //driver.quit();
+
     }
 }
